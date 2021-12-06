fac2a3dd44ce31dfb50e633353db29263141bd1e
JoeSachin11 <85384411+JoeSachin11@users.noreply.github.com>
2021-12-06 10:54:44 +0530

    Update utils.dart

--- My Cars/lib/utils/utils.dart
+++ My Cars/lib/utils/utils.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const MainHeading = TextStyle(fontWeight: FontWeight.bold, fontSize: 30);
+const MainHeading = TextStyle(fontWeight: FontWeight.bold, fontSize: 20);
 
-const SubHeading = TextStyle(fontWeight: FontWeight.bold, fontSize: 20);
+const SubHeading = TextStyle(fontWeight: FontWeight.bold, fontSize: 15);
 
-const BasicHeading = TextStyle(fontSize: 15);
+const BasicHeading = TextStyle(fontSize: 10);
